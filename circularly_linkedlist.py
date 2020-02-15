714b1b4c27bf0a3a355d1f03fecf76b77f433cba
anto-pravin <59083077+anto-pravin@users.noreply.github.com>
2020-02-15 23:05:07 +0530

    upload

---
+++ circularly_linkedlist.py
@@ -0,0 +1,62 @@
+class node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class circularly_linkedlist(node):
+    def __init__(self):
+        self.head = None
+
+    def insertfirst(self, data):
+        newnode = node(data)
+        if not self.head:
+            self.head = newnode
+            newnode.next = self.head
+
+        else:
+            newnode.next = self.head
+            self.head = newnode
+
+    def insertlast(self, data):
+        newnode = node(data)
+        if not self.head:
+            self.insertfirst(data)
+        else:
+            actualnode = self.head
+            while actualnode.next is not self.head:
+                actualnode = actualnode.next
+            actualnode.next = newnode
+            newnode.next = self.head
+
+    def insert(self, data):
+        self.insertlast(data)
+
+    def disp(self):
+        actualnode = self.head
+        l = []
+        while actualnode.next is not self.head:
+            l.append(actualnode.data)
+            actualnode = actualnode.next
+        l.append(actualnode.data)
+        return l
+
+    def shift(self, data):
+        actualnode = self.head
+        l = []
+        for  i in range(data):
+            actualnode = actualnode.next
+        stop = actualnode
+        while stop.next is not actualnode:
+            l.append(stop.data)
+            stop = stop.next
+        l.append(stop.data)
+        return l
+
+
+n = int(input())
+k = list(map(int,input().split()))
+s = int(input())
+a = circularly_linkedlist()
+for i in k:
+    a.insert(i)
+print(*a.shift(s)[::-1])
